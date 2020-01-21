252be5343c5baf9298755bba857ec7b2cfc49540
Florian Angeletti <florian.angeletti@inria.fr>
2020-01-21 18:02:38 +0100

    Alternative patch

--- src/exn_stubs.c
+++ src/exn_stubs.c
@@ -1,20 +1,10 @@
+#define CAML_NAME_SPACE
 #include <caml/mlvalues.h>
-#include <caml/version.h>
-
-#if OCAML_VERSION >= 41000
-
-CAMLprim value Base_clear_caml_backtrace_pos () {
-  Caml_state_field(backtrace_pos) = 0;
-  return Val_unit;
-}
-
-#else
-
-extern int caml_backtrace_pos;
+#define CAML_INTERNALS
+#include <caml/backtrace.h>
+#undef CAML_INTERNALS
 
 CAMLprim value Base_clear_caml_backtrace_pos () {
   caml_backtrace_pos = 0;
   return Val_unit;
 }
-
-#endif
